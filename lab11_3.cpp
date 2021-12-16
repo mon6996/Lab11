8f65154aecec9999dd31766b9d0b24d152954356
M <thanaporn_chan@cmu.ac.th>
2021-12-16 12:11:53 +0700

    11_3 complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,28 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    float sum = 0, sum2 = 0;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source, textline))
+    {
+        sum += atof(textline.c_str());
+        sum2 += pow(atof(textline.c_str()), 2);
+        count++;
+    }
+    source.close();
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << "\n";
+    cout << "Standard deviation = " << sqrt(sum2/count - pow(sum/count, 2)) << "\n";
+    return 0;
+}
