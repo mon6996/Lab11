6d5e6027504df7037f31ccd7e600d2f671c632f1
M <thanaporn_chan@cmu.ac.th>
2021-12-16 11:42:13 +0700

    11_2 complete

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+
 using namespace std;
 
-int main (){
+int main ()
+{
 	ifstream source;
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string textline;
+	dest << "-------------------- BOOM ---------------------\n";
+	while(getline(source, textline))
+	{
+		dest << textline << "\n";
+	}
+	dest << "-------------------- HA!! ---------------------";
 
     source.close();
     dest.close();
